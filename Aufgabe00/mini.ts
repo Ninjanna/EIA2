f26d964828f42e4138f34eb8e4343b4e53c1f8d4
jeannanechypurenko <jeannanechypurenko@jeannas-mbp.eduroam.hs-furtwangen.de>
2018-03-22 17:16:03 +0100

    miniseite up

---
+++ Aufgabe00/mini.ts
@@ -0,0 +1,14 @@
+/*  Name: Jeanna nechypurenko
+Matrikel: 257705
+Datum: 22.Mrz18
+
+Hiermit versichere ich, dass ich diesen
+Code selbst geschrieben habe. Er wurde
+nicht kopiert und auch nicht diktiert.*/ 
+
+function Welcome() {
+  var name = prompt("Hey sweetie, what's your name?", "");
+    document.getElementById("Blub").innerHTML =
+      "Willkommen " + name + " :) ";  
+}
+
