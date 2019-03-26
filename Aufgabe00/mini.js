7b08dee572eee9c001b448da5b1ba555912a4b1c
Jeanna <Jeanna@Andriys-MacBook-Pro.fritz.box>
2019-03-26 18:51:14 +0100

    Event listener

--- Aufgabe00/mini.js
+++ Aufgabe00/mini.js
@@ -5,10 +5,12 @@ Datum: 22.Mrz19
 Hiermit versichere ich, dass ich diesen
 Code selbst geschrieben habe. Er wurde
 nicht kopiert und auch nicht diktiert.*/
+document.addEventListener("DOMContentLoaded", welcome);
 function welcome() {
     var name = prompt("Hey sweetie, what's your name?", "");
     document.getElementById("Blub").innerHTML =
         "Willkommen " + name + " :) ";
     console.log("Willkommen " + name +  "! Ich bin eine Konsolenausgabe :)");
 }
+
 //# sourceMappingURL=mini.js.map 
